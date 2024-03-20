ebabf3ee81aa974113acc562cbf42baf254b5a0b
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-03-20 15:05:52 -0700

    created "directories.zsh" to store directories in variables

---
+++ scripts/directories.zsh
@@ -0,0 +1,4 @@
+readonly REC_POSTPRODUCTION_DIR=(~/Library/CloudStorage/GoogleDrive*/Shared\ drives/REC_POST)
+
+readonly gDriveScriptsDir="$(realpath ${REC_POSTPRODUCTION_DIR}/REC_SCRIPTS.lnk)"
+readonly localScriptsDir="${0:A:h:h}/src"
