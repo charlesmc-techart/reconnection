d75e36edba9e873f499b4f87321d8183164676d9
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-03-19 23:06:48 -0700

    moved func "exportGeometryCache" from module "objects" to "export_geometryCaches"

--- src/rec/modules/maya/objects.py
+++ src/rec/modules/maya/objects.py
@@ -90,57 +90,6 @@ def exportNodes(
         )
 
 
-def exportGeometryCache(dir: Path, fileName: str) -> None:
-    """Call the MEL procedure for exporting a geometry cache"""
-    # global proc stirng[] doCreateGeometryCache( int $version, string $args[] )
-    #     $version == 1:
-    #         $args[0] =  time range mode:
-    #             time range mode = 0 : use $args[1] and $args[2] as start-end
-    #             time range mode = 1 : use render globals
-    #             time range mode = 2 : use timeline
-    #          $args[1] = start frame (if time range mode == 0)
-    #          $args[2] = end frame (if time range mode == 0)
-
-    #     $version == 2:
-    #          $args[3] = cache file distribution, either "OneFile"
-    #                     or "OneFilePerFrame"
-    #          $args[4] = 0/1, whether to refresh during caching
-    #          $args[5] = directory for cache files, if "", then use project
-    #                     data dir
-    #          $args[6] = 0/1, whether to create a cache per geometry
-    #          $args[7] = name of cache file. An empty string can be used to
-    #                     specify that an auto-generated name is acceptable.
-    #          $args[8] = 0/1, whether the specified cache name is to be used
-    #                     as a prefix
-
-    #     $version == 3:
-    #          $args[9] = action to perform: "add", "replace", "merge",
-    #                     "mergeDelete" or "export"
-    #         $args[10] = force save even if it overwrites existing files
-    #         $args[11] = simulation rate, the rate at which the cloth
-    #                     simulation is forced to run
-    #         $args[12] = sample mulitplier, the rate at which samples are
-    #                     written, as a multiple of simulation rate.
-
-    #     $version == 4:
-    #         $args[13] = 0/1, whether modifications should be inherited
-    #                     from the cache about to be replaced. Valid
-    #                     only when $action == "replace".
-    #         $args[14] = 0/1, whether to store doubles as floats
-
-    #     $version == 5:
-    #         $args[15] = name of cache format
-
-    #     $version == 6:
-    #         $args[16] = 0/1, whether to export in local or world space
-    doCreateGeometryCacheCmd = (
-        f'doCreateGeometryCache 6 {{"2", "1", "10", "OneFile", "0", '
-        f'"{dir.as_posix()}", "0", "{fileName}", "0", "export", '
-        '"1", "1", "1", "0", "0", "mcx", "1"}'
-    )
-    mel.eval(doCreateGeometryCacheCmd)
-
-
 def importGeometryCache(
     filePath: Path, assetName: fname.AssetName, namespace: str
 ) -> None:
