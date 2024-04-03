812d9d72b7c75136b96a103f4eda4af38545692c
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-03 05:20:30 -0700

    Removes cmd in filename

--- src/rec_cmd_render.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-SETLOCAL
-set /A MAYA_VER=2023
-ENDLOCAL
-
-"C:\Program Files\Autodesk\Maya%MAYA_VER%\bin\mayapy" "%~dp0\rec\render.py"
---
+++ src/rec_render.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\Autodesk\Maya2023\bin\mayapy" "%~dp0\rec\render.py"
