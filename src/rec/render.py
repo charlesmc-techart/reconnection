311a22f93001f3a71b3cfcd17ec4fba6884ca566
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-09 00:54:11 -0700

    Adds arnold batch render

--- src/rec/render.py
+++ src/rec/render.py
@@ -1,54 +1,26 @@
 #!/Applications/Autodesk/maya2023/Maya.app/Contents/bin/mayapy
 
-import csv
 import os
 import shutil
 import subprocess
 import sys
-from collections import deque
-from functools import partial
 from pathlib import Path
-from time import sleep
+
+import rec.modules.files.queue as fqueue
 
 _SCRIPTS_DIR = Path(__file__).parents[1]
-_RENDER_QUEUE = _SCRIPTS_DIR / "render_queue.txt"
-
-_openQueueCmd = partial(_RENDER_QUEUE.open, encoding="utf-8")
-_readQueueCmd = partial(_openQueueCmd, "r")
-_writeQueueCmd = partial(_openQueueCmd, "w")
+_RENDER_QUEUE = _SCRIPTS_DIR / "__render_queue.txt"
 
 
-def readTxtQueue() -> deque[str]:
-    with _readQueueCmd() as f:
-        return deque(f)
+def renderFlair(scene: str) -> None: ...
 
 
-def updateTxtQueue(queue: deque[str]) -> None:
-    with _writeQueueCmd() as f:
-        f.writelines(queue)
-
-
-def readCsvQueue() -> deque[list[str]]:
-    with _readQueueCmd() as f:
-        reader = csv.reader(f)
-        return deque(reader)
-
-
-def updateCsvQueue(queue: deque[list[str]]) -> None:
-    with _writeQueueCmd() as f:
-        writer = csv.writer(f)
-        writer.writerows(queue)
-
-
-def renderFlair(scene: Path) -> None: ...
-
-
-def renderArnold(scene: Path) -> None:
+def renderArnold(scene: str) -> None:
     mayaPath = os.path.join(os.environ["MAYA_LOCATION"], "bin")
     if mayaPath not in sys.path:
         sys.path.insert(0, mayaPath)
 
-    args = ("Render", scene)
+    args = ("Render", "-renderer", "arnold", scene)
     results = subprocess.run(args, capture_output=True, text=True)
     print(results)
 
@@ -58,19 +30,15 @@ def main():
     # Backup the queue
     shutil.copy(_RENDER_QUEUE, f"{_RENDER_QUEUE}.bak")
 
-    queue = readTxtQueue()
+    queue = fqueue.readTxtQueue(_RENDER_QUEUE)
 
     while queue:
-        filePath = queue.popleft()
+        scene = queue.popleft()
 
-        try:
-            print(f"{filePath!r}")
-        except Exception:
-            raise
+        print(f"Rendering: {scene!r}")
+        renderArnold(scene=scene)
 
-        updateTxtQueue(queue)
-
-        sleep(0.5)
+        fqueue.updateTxtQueue(_RENDER_QUEUE, queue=queue)
 
 
 if __name__ == "__main__":
