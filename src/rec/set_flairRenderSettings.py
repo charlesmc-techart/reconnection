428049e7bd16875b4b5e0460a27197707af189fe
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-11 02:03:12 -0700

    Tries to set Flair render settings

--- src/rec/set_flairRenderSettings.py
+++ src/rec/set_flairRenderSettings.py
@@ -1,15 +1,31 @@
 import maya.cmds as cmds
 
+import rec.modules.files.names as fname
+import rec.modules.files.paths as fpath
 import rec.modules.maya as mapp
 
 
 @mapp.logScriptEditorOutput
 def main() -> None:
-    cmds.flair()  # FIXME: set name
+    shot = fname.ShotID.fromFilename(fpath.getScenePath().stem)
 
-    cmds.flair()  # FIXME: set quality to TAA
-    cmds.flair(alpha=2)
-    cmds.flair()  # FIXME: set format to EXR
+    fg = "flairGlobals"
+    try:  # cmds.flair()  # FIXME: set name
+        cmds.setAttr(f"{fg}._sequenceName", f"{shot.name.upper()}.<####>")
+    except Exception as e:
+        cmds.warning(e)
+
+    try:  # cmds.flair()  # FIXME: set quality to TAA
+        cmds.setAttr(f"{fg}._taa", True)
+    except Exception as e:
+        cmds.warning(e)
+
+    cmds.flair(alpha=2)  # set alphat to Premult
+
+    try:  # cmds.flair()  # FIXME: set format to EXR
+        cmds.setAttr(f"{fg}._format", ".exr")
+    except Exception as e:
+        cmds.warning(e)
 
     targets = (
         "cryptomatte",
@@ -22,7 +38,17 @@ def main() -> None:
     )
     cmds.flair(target=targets)
 
-    cmds.flair()  # FIXME: set render scale to 100%
+    try:  # cmds.flair()  # FIXME: set render scale to 100%
+        cmds.setAttr(f"{fg}._renderScale", 1)
+    except Exception as e:
+        cmds.warning(e)
 
-    cmds.flair()  # FIXME: set render each light to True
-    cmds.flair()  # FIXME: set bundle AOVs in EXR to True
+    try:  # cmds.flair()  # FIXME: set render each light to True
+        cmds.setAttr(f"{fg}._eachLight", True)
+    except Exception as e:
+        cmds.warning(e)
+
+    try:  # cmds.flair()  # FIXME: set bundle AOVs in EXR to True
+        cmds.setAttr(f"{fg}._bundleAOVs", True)
+    except Exception as e:
+        cmds.warning(e)
