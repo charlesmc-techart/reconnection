09520b24caff0145de69a6dd75bbc54e1f02219f
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-18 02:35:07 -0700

    Calls render.py from Zsh / Cmd

--- src/rec/render.py
+++ src/rec/render.py
@@ -1,69 +1,51 @@
 #!/Applications/Autodesk/maya2023/Maya.app/Contents/bin/mayapy
+"""Batch render scenes from a queue; to be called by Cmd or Zsh"""
 
 import os
 import shutil
-import subprocess
-import traceback
-from collections import deque
-from functools import partial
-from pathlib import Path
 
 import rec.modules.queue as mqueue
 
-_SCRIPTS_DIR = Path(__file__).parents[1]
-_RENDER_QUEUE = _SCRIPTS_DIR / "__render_queue.txt"
-_FAILED_QUEUE = _SCRIPTS_DIR / "__render_queue_failed.csv"
 
-
-runCmd = partial(subprocess.run, capture_output=True, text=True)
-
-
-def renderFlair(scene: str) -> None:
-    args = (
-        "mayabatch",
-        "-file",
-        scene,
-        "-command",
-        'python(""Import flair_batch"")',
-        "-noAutoloagPlugins",
-    )
-    results = runCmd(args)
-    print(results)
-
-
-def renderArnold(scene: str) -> None:
-    args = ("Render", "-renderer", "arnold", scene)
-    results = runCmd(args)
-    print(results)
-
-
-# @mapp.logScriptEditorOutput
 def main():
-    mayaPath = os.path.join(os.environ["MAYA_LOCATION"], "bin")
-    os.environ["PATH"] += f"{os.pathsep}{mayaPath}"
+    shutil.copy(mqueue.RENDER_QUEUE, f"{mqueue.RENDER_QUEUE}~")
 
-    # Backup the queue
-    shutil.copy(_RENDER_QUEUE, f"{_RENDER_QUEUE}~")
+    queue = mqueue.readTxtQueue(mqueue.RENDER_QUEUE)
 
-    queue = mqueue.readTxtQueue(_RENDER_QUEUE)
-    failedQueue: deque[tuple[str, str]] = deque()
-
-    while queue:
-        scene = queue.popleft().strip()
-
-        print(f"Rendering: {scene!r}")
+    while True:
         try:
-            if "arnold" in os.path.basename(scene):
-                renderArnold(scene=scene)
-            else:
-                renderFlair(scene=scene)
-        except Exception as e:
-            traceback.print_exc()
+            scene = queue.popleft().strip()
+        except IndexError:  # Queue is empty
+            return
+        if os.path.isfile(scene):
+            break
+        with mqueue.FAILED_QUEUE.open("a", encoding="utf8") as f:
+            f.write(scene)
 
-            failedQueue.append((scene, f"{e}"))
-            mqueue.updateCsvQueue(_FAILED_QUEUE, queue=failedQueue)  # type: ignore
+    if "arnold" in os.path.basename(scene):
+        print(
+            os.path.join(os.environ["MAYA_LOCATION"], "bin", "Render"),
+            "-renderer",
+            "arnold",
+            "-ai:aerr",
+            "true",
+            "-ai:alf",
+            "true",
+            scene,
+            sep=",",
+        )
+    else:
+        print(
+            os.path.join(os.environ["MAYA_LOCATION"], "bin", "mayabatch"),
+            "-file",
+            scene,
+            "-command",
+            'python(""Import flair_batch"")',
+            "-noAutoloagPlugins",
+            sep=",",
+        )
 
-        mqueue.updateTxtQueue(_RENDER_QUEUE, queue=queue)
+    mqueue.updateTxtQueue(mqueue.RENDER_QUEUE, queue=queue)
 
 
 if __name__ == "__main__":
