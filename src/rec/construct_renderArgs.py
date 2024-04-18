1840ef2b4f15f5844589a280e44c17490c401e5a
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-18 15:42:35 -0700

    Makes mayapy script construct the args needed by Cmd / Zsh to batch render

--- src/rec/construct_renderArgs.py
+++ src/rec/construct_renderArgs.py
@@ -1,9 +1,10 @@
 #!/Applications/Autodesk/maya2023/Maya.app/Contents/bin/mayapy
-"""Batch render a Maya scene from the queue; to be called by Cmd or Zsh"""
+"""Construct the args needed to batch render with Cmd or Zsh"""
 
 import os
 import shutil
 from pathlib import Path
+from argparse import ArgumentParser
 
 import rec.modules.queue as mqueue
 
@@ -14,7 +15,7 @@ RENDER_QUEUE = _SCRIPTS_DIR / "__render_queue.txt"
 FAILED_TO_RENDER = _SCRIPTS_DIR / "__render_queue_failed.csv"
 
 
-def main():
+def main(scriptFile: Path) -> None:
     shutil.copy(RENDER_QUEUE, f"{RENDER_QUEUE}~")
 
     queue = mqueue.readTxtQueue(RENDER_QUEUE)
@@ -22,16 +23,18 @@ def main():
         try:
             scene = queue.popleft().strip()
         except IndexError:  # Queue is empty
-            return
+            exit(1)  # Break out of the while loop in Cmd / Zsh
         if os.path.isfile(scene):
             scene = Path(scene)
             break
         with FAILED_TO_RENDER.open("a", encoding="utf8") as f:
             f.write(f"{scene}")
 
+    # mayaPath = os.path.join(os.environ["MAYA_LOCATION"], "bin")
+    mayaPath = "/Applications/Autodesk/maya2023/Maya.app/Contents/bin"
     if "arnold" in scene.stem:
-        print(
-            os.path.join(os.environ["MAYA_LOCATION"], "bin", "Render"),
+        args = (
+            os.path.join(mayaPath, "Render"),
             "-renderer",
             "arnold",
             "-ai:aerr",
@@ -39,21 +42,28 @@ def main():
             "-ai:alf",
             "true",
             scene,
-            sep=",",
         )
     else:
-        print(
-            os.path.join(os.environ["MAYA_LOCATION"], "bin", "mayabatch"),
+        args = (
+            os.path.join(mayaPath, "mayabatch"),
             "-file",
             scene,
             "-command",
             'python(""Import flair_batch"")',
             "-noAutoloadPlugins",
-            sep=",",
         )
+    with scriptFile.open("w", encoding="utf-8") as f:
+        print(*args, file=f)
 
     mqueue.updateTxtQueue(RENDER_QUEUE, queue=queue)
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(
+        prog="re:connection Batch Render Args Constructor", description=__doc__
+    )
+    parser.add_argument(
+        "file", type=Path, help="path to script file the shell will execute"
+    )
+    args = parser.parse_args()
+    main(args.file)
--- src/rec_render.cmd
+++ src/rec_render.cmd
@@ -1,1 +1,15 @@
-"C:\Program Files\Autodesk\Maya2023\bin\mayapy" "%~dp0\rec\render.py"
+@echo off
+:: Batch render Maya scenes from the queue
+
+set "SCRIPT_DIR=%~dp0"
+set "SCRIPT=%SCRIPT_DIR%\rec\construct_renderArgs.py"
+set "CMD=%SCRIPT_DIR%\__render.cmd"
+
+set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
+
+:loop
+"%MAYAPY%" "%SCRIPT%" "%CMD%"
+if errorlevel 1 goto :eof
+
+call "%CMD%"
+goto :loop
