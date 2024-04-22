3466fd498322748a154d071184e0bf9d3089b7d1
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-22 01:41:18 -0700

    Minor formatting fix

--- src/rec_render.cmd
+++ src/rec_render.cmd
@@ -1,11 +1,9 @@
 @echo off
 :: Batch render Maya scenes from the queue
 
-set "SCRIPT_DIR=%~dp0"
-set "SCRIPT=%SCRIPT_DIR%\rec\construct_renderArgs.py"
-set "CMD=%SCRIPT_DIR%\__render.cmd"
-
 set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
+set "SCRIPT=%~dp0\rec\construct_renderArgs.py"
+set "CMD=%~dp0\__render.cmd"
 
 :loop
 "%MAYAPY%" "%SCRIPT%" "%CMD%"
