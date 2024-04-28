b4a04b800aedb2470062593bcb42e2071b5fe2c3
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-27 22:42:15 -0700

    Revert "Updates cmd script"
    This reverts commit 61c105215face5566b3df4ff66ca85e3214acd6d.

--- src/rec_render.cmd
+++ src/rec_render.cmd
@@ -2,10 +2,11 @@
 
 set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
 set "SCRIPT=%~dp0rec\renderArgs.py"
+set "CMD=%~dp0__render_args.cmd"
 
 :loop
 "%MAYAPY%" "%SCRIPT%"
 if errorlevel 1 goto :eof
 
-"%CMD%"
+call "%CMD%"
 goto :loop
