61c105215face5566b3df4ff66ca85e3214acd6d
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-27 22:38:34 -0700

    Updates cmd script

--- src/rec_render.cmd
+++ src/rec_render.cmd
@@ -1,13 +1,11 @@
-@echo off
 :: Batch render Maya scenes from the queue
 
 set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
-set "SCRIPT=%~dp0\rec\renderArgs.py"
-set "CMD=%~dp0\__render_args.cmd"
+set "SCRIPT=%~dp0rec\renderArgs.py"
 
 :loop
-"%MAYAPY%" "%SCRIPT%" "%CMD%"
+"%MAYAPY%" "%SCRIPT%"
 if errorlevel 1 goto :eof
 
-call "%CMD%"
+"%CMD%"
 goto :loop
