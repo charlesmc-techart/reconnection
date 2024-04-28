5d4843fc93d21494eef1e6cff652e27e6fcfe647
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-04-27 23:09:42 -0700

    Removes \

--- src/rec_export_mechanicGeometryCaches.cmd
+++ src/rec_export_mechanicGeometryCaches.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
-set "SCRIPT=%~dp0\rec\export_cachesCamera_queue.py" "%*"
+set "SCRIPT=%~dp0rec\export_cachesCamera_queue.py" "%*"
 
 "%MAYAPY%" "%SCRIPT%"
