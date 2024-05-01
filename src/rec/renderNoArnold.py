36214697eef4064406ba124e01bb240754a322b8
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-05-01 16:07:44 -0700

    Renders just Flair

---
+++ src/rec/renderNoArnold.py
@@ -0,0 +1,63 @@
+#!/Applications/Autodesk/maya2023/Maya.app/Contents/bin/mayapy
+"""Batch render using Flair"""
+
+__author__ = "Charles Mesa Cayobit"
+
+import os
+import shutil
+import subprocess
+import sys
+from functools import partial
+from pathlib import Path
+
+_SCRIPTS_DIR = Path(__file__).parents[1]
+sys.path.insert(0, f"{_SCRIPTS_DIR}")
+
+import rec.modules.files.paths as fpath
+import rec.modules.queue as mqueue
+
+RENDER_QUEUE = _SCRIPTS_DIR / "__render_queue.txt"
+FAILED_TO_RENDER = _SCRIPTS_DIR / "__render_failed.txt"
+
+
+def render(scene: Path) -> None:
+    mayaPath = os.path.join(os.environ["MAYA_LOCATION"], "bin", "mayabatch")
+    args = (
+        mayaPath,
+        "-file",
+        scene.as_posix(),
+        "-proj",
+        fpath.findSharedDrive().as_posix(),
+        "-command",
+        'python("import rec.renderFlair")',
+        "-noAutoloadPlugins",
+    )
+
+    subprocess.run(args, stdout=sys.stdout, stderr=sys.stderr)
+
+
+def main() -> None:
+    border = partial(print, "", "#" * 80, "", sep="\n")
+    border()
+
+    shutil.copy(RENDER_QUEUE, f"{RENDER_QUEUE}~")
+
+    print("Starting render...", "", sep="\n")
+
+    queue = mqueue.readTxt(RENDER_QUEUE)
+    while queue:
+        scene = queue.popleft().strip()
+        if os.path.isfile(scene):
+            render(Path(scene))
+        else:
+            with FAILED_TO_RENDER.open("a", encoding="utf8") as f:
+                print(scene, file=f)
+
+        mqueue.updateTxt(RENDER_QUEUE, queue=queue)
+
+    print("", "Render done!", sep="\n")
+    border()
+
+
+if __name__ == "__main__":
+    main()
--- src/rec_render.cmd
+++ src/rec_render.cmd
@@ -1,12 +1,8 @@
 :: Batch render Maya scenes from the queue
 
+@echo off
 set "MAYAPY=C:\Program Files\Autodesk\Maya2023\bin\mayapy"
-set "SCRIPT=%~dp0rec\renderArgs.py"
-set "CMD=%~dp0__render_args.cmd"
+set "SCRIPT=%~dp0rec\renderNoArnold.py"
 
-:loop
+@echo on
 "%MAYAPY%" "%SCRIPT%"
-if errorlevel 1 goto :eof
-
-"%CMD%"
-goto :loop
