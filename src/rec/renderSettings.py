a9c8e80c812304b992d914f63213fd258e8d23d8
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2024-05-10 16:07:10 -0700

    Temporary settings for playblast

--- src/rec/renderSettings.py
+++ src/rec/renderSettings.py
@@ -1,8 +1,13 @@
 """Set render settings"""
 
+from __future__ import annotations
+
 __author__ = "Charles Mesa Cayobit"
 
+from typing import ClassVar
+
 import maya.cmds as cmds
+import maya.mel as mel
 
 import rec.modules.files.names as fname
 import rec.modules.files.paths as fpath
@@ -35,6 +40,72 @@ def setGlobals() -> None:
     endFrame = cmds.getAttr(f"{cache}.originalEnd")
     cmds.playbackOptions(maxTime=endFrame, animationEndTime=endFrame)
     cmds.setAttr(f"{drg}.endFrame", endFrame)
+
+
+class setPlayblast:
+    """Temporarily set playblast settings"""
+
+    modelEditorOptions: ClassVar[dict[str, bool | str]] = {
+        "allObjects": False,
+        "displayAppearance": "smoothShaded",
+        "displayLights": "flat",
+        "displayTextures": False,
+        "grid": False,
+        "headsUpDisplay": False,
+        "manipulators": False,
+        "polymeshes": True,
+        "useDefaultMaterial": False,
+        "selectionHiliteDisplay": False,
+        "shadows": False,
+        "wireframeOnShaded": False,
+        "xray": False,
+    }
+    hardwareRenderGlobalsOptions: ClassVar[dict[str, bool]] = {
+        "ssaoEnable": False,
+        "motionBlurEnable": False,
+        "multiSampleEnable": True,
+    }
+
+    def __init__(self) -> None:
+        self.viewport: str = cmds.playblast(activeEditor=True)
+
+        self.origModelEditorOptions: dict[str, bool | str] = {}
+        for k in self.modelEditorOptions:
+            if k == "allObjects":
+                self.origModelEditorOptions[k] = True
+            else:
+                self.origModelEditorOptions[k] = cmds.modelEditor(
+                    self.viewport, query=True, **{k: True}
+                )
+
+        self.origHardwareRenderGlobalsOptions: dict[str, bool] = {
+            k: cmds.getAttr(f"hardwareRenderingGlobals.{k}")
+            for k in self.hardwareRenderGlobalsOptions
+        }
+        self.origGradient: bool = cmds.displayPref(
+            query=True, displayGradient=True
+        )
+        self.origBg = cmds.displayRGBColor("background", query=True)
+
+    def __enter__(self) -> str:
+        cmds.modelEditor(self.viewport, edit=True, **self.modelEditorOptions)
+        for k, v in self.hardwareRenderGlobalsOptions.items():
+            cmds.setAttr(f"hardwareRenderingGlobals.{k}", v)
+        cmds.displayPref(displayGradient=False)
+        cmds.displayRGBColor("background", 0, 0, 0)
+
+        return self.viewport
+
+    def __exit__(self, *args) -> None:
+        cmds.modelEditor(
+            self.viewport, edit=True, **self.origModelEditorOptions
+        )
+        for k, v in self.origHardwareRenderGlobalsOptions.items():
+            cmds.setAttr(f"hardwareRenderingGlobals.{k}", v)
+        cmds.displayPref(displayGradient=self.origGradient)
+        cmds.displayRGBColor("background", *self.origBg)
+
+        mel.eval("rebuildShowMenu")
 
 
 @mapp.logScriptEditorOutput
